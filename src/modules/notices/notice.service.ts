54f398adb9d3110debe736f1eebd6df70d185cce
developerMohib <mohibullahmohim2020@gmail.com>
2026-01-11 14:43:24 +0600

    in services, call db for check buffuring

--- src/modules/notices/notice.service.ts
+++ src/modules/notices/notice.service.ts
@@ -1,21 +1,27 @@
+import { connectDB } from '../../db/db';
 import { Notice } from './notice.model';
 
-export const createNotice = (payload: any) => {
+export const createNotice = async (payload: any) => {
+  await connectDB();
   return Notice.create(payload);
 };
 
-export const getAllNotices = () => {
+export const getAllNotices = async () => {
+  await connectDB();
   return Notice.find().sort({ createdAt: -1 });
 };
 
-export const getSingleNotice = (id: string) => {
+export const getSingleNotice = async (id: string) => {
+  await connectDB();
   return Notice.findById(id);
 };
 
-export const updateNotice = (id: string, payload: any) => {
+export const updateNotice = async (id: string, payload: any) => {
+  await connectDB();
   return Notice.findByIdAndUpdate(id, payload, { new: true });
 };
 
-export const deleteNotice = (id: string) => {
+export const deleteNotice = async (id: string) => {
+  await connectDB();
   return Notice.findByIdAndDelete(id);
 };
